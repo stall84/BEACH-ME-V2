be3f3780a97394f71db6b35996dc72a1adc0a6dd
Michael Stallings <stall84@gmail.com>
2020-06-25 21:14:40 -0400

    Added forecaster component with boilerplate

---
+++ src/components/Forecaster.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class Forecaster extends Component {
+    
+    
+    
+    
+    
+    render() {
+
+
+        return (
+            <div>
+                HI! 
+                <br/>
+                ONE AWESOME BEACH-ME APP OTW
+
+            </div>
+        )
+    }
+}
+
+export default Forecaster;
