78ff8768921087e14440d5a16933f1c645e3f89c
Michael Stallings <stall84@gmail.com>
2020-07-11 14:01:14 -0400

    Created axios calls to backend in landing pages comp did mount. Need to refactor as much api call logic out of front end, set development proxy to backend

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,6 +5,7 @@ import Carousel from './Carousel';
 import Footer from './Footer';
 import Header from './Header';
 import './styles/LandingPage.css';
+import axios from 'axios';
 
 
 class LandingPage extends Component {
@@ -27,18 +28,51 @@ class LandingPage extends Component {
                     latitude: position.coords.latitude,
                     longitude: position.coords.longitude
                 })
+                axios.post('/api/v1/beaches', {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                })
+                    .then((response) => {
+                    console.log('Response from POST call to backend: ', response)
+                })
+                    .catch(error => {
+                        console.log('Error posting coords: ', error)
+                    })
+
              
              // This method of pushing the user's lat/long into state is working during 
              // initial tests. Things I've found online though make me think it's an async call
              // and might need some other proving logic other than the if statement at very beginning 
-             this.setState({
-                 lat: position.coords.latitude,
-                 lng: position.coords.longitude
-             })
+            //  this.setState({
+            //      lat: position.coords.latitude,
+            //      lng: position.coords.longitude
+            //  })
                 
         
        });
     }
+    }
+
+    // componentDidUpdate(prevState) {
+       
+    //     if (this.state !== prevState) {
+    //         console.log('State updated.. ')
+    //             // let origins = {
+    //             //     lat: this.state.lat,
+    //             //     lng: this.state.lng
+    //             // };
+    //             let lat = this.state.lat;
+    //             let lng = this.state.lng;
+                
+
+    //     }
+    //   }
+
+      checkAPI = () => {
+        axios.get('/api/v1/beaches')
+            .then(response => {
+                console.log(response)
+            })
     }
 
     render() {
@@ -53,6 +87,7 @@ class LandingPage extends Component {
                 <h2>Click below to get BEACHED!</h2>
                 <div className="inputButtons">
                     <Link to="/main-map"><button type="button" className="btn btn-outline-warning btn-lg">PLZ BEACH ME!</button></Link>
+                    <button type="button" onClick={this.checkAPI}>CHECK API</button>
                 </div>
                 <Footer />
             </div>
